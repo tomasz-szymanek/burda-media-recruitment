601f2cba3d234bbcfcc64328d4929eda76b608f3
Tomasz Szymanek tomasz.szymanek@gmail.com <tomasz.szymanek@gmail.com>
2022-12-13 09:23:02 +0100

    types

---
+++ app/OptimalShoppingList/types/interfaces.ts
@@ -0,0 +1,5 @@
+export interface Item {
+  name: string;
+  review_rating: number;
+  price: number;
+}
---
+++ app/types/index.ts
@@ -0,0 +1,5 @@
+const CONFIGURATION_TYPES = {
+  Config: Symbol.for("Config"),
+};
+
+export { CONFIGURATION_TYPES };
